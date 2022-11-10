5c05585357528c3d6dea564eeacd476f0fa9bb4c
guilherme3110 <61066086+guilherme3110@users.noreply.github.com>
2022-11-10 13:11:42 -0300

    catalogo geek

--- script.js
+++ script.js
@@ -1,29 +1,107 @@
-/* 
+const listaItens = [
+  {
+    image: 'assets/img/painting/clock.jpg',
+    name: 'clock',
+    price: 10,
+    type: 'quadro',
+  },
+  {
+    image: 'assets/img/painting/gamepad.jpg',
+    name: 'clock',
+    price: 12,
+    type: 'quadro',
+  },
+  {
+    image: 'assets/img/painting/personagem.jpg',
+    name: 'clock',
+    price: 14,
+    type: 'quadro',
+  },
+  {
+    image: 'assets/img/actions/animewoman.jpg',
+    name: 'animewoman',
+    price: 20,
+    type: 'acao',
+  },
+  {
+    image: 'assets/img/actions/dragonballpersonagem.jpg',
+    name: 'dragonball',
+    price: 22,
+    type: 'acao',
+  },
+  {
+    image: 'assets/img/actions/starwarspersonagem.jpg',
+    name: 'starwars',
+    price: 24,
+    type: 'acao',
+  }
+];
 
-- DOM ( Modelo de objeto do documento)
--window.document
-  -window.document.write('ola mundo)
-  -window.document.body.style = "background;"
-  -document.title = titulo da pagina";
-  -window.location
-  -window.location. href
+let listFigures = [];
+let listFrames = [];
 
-  -Seleltores 
-    - querySelector();
-        -tag('tag)
-        - class('.classe')
-        - id('#id')
-*/        
+function separaItens(listaItens){
+  for(let i = 0;i < listaItens.length; i++){
+      if(listaItens[i].type == 'quadro' ) {
+       listFrames.push(listaItens[i])
+      }else if(listaItens[i].type == "acao"){
+        listFigures.push(listaItens[i])
+      }
+  } 
+  
+}
 
-// CreateElement();
+separaItens(listaItens)
 
+let secaoListPaintings = document.querySelector('.paintingsUl')
+  for( let i = 0 ;i < listFrames.length; i ++){
+    let liFrames = document.createElement('li')
 
-let p = document.createElement('p'); // criamos a tag de paragrafo
-console.log(p);
+    let boxItem = document.createElement('div')
+    liFrames.appendChild(boxItem)
 
-p.innerHTML = "Esse é um texto de teste";
-console.log(p);
+    let img = document.createElement('img')
+    img.src = listFrames[i].image
 
-p.setAttribute('name', 'paragrafo');
+    let titulo = document.createElement('h3')
+    titulo.classList = 'color'
+    titulo.innerHTML = `<strong>${listFrames[i].name} </strong>` 
 
+    let p = document.createElement('p')
+    p.classList = 'colorValor'
+    p.innerText = `R$ ${listFrames[i].price},00`
+
+    boxItem.appendChild(img)
+    boxItem.appendChild(titulo)
+    boxItem.appendChild(p)
+    liFrames.appendChild(boxItem)
+    secaoListPaintings.appendChild(liFrames)
+  }
+
+  let secaolistFigure = document.querySelector('.actionFiguresUl')
+  
+   for( let i = 0; i < listFigures.length; i++){
+      let liFigures = document.createElement('li')
+
+      let boxFigures = document.createElement('div')
+
+      liFigures.appendChild(boxFigures)
+
+      let imgFigures = document.createElement('img')
+      imgFigures.src = listFigures[i].image
+
+      let tituloFigures = document.createElement('h3')
+      tituloFigures.classList = 'color'
+      tituloFigures.innerHTML = '${listFigures[i].name}'
+
+      let pFigures = document.createElement('p')
+      pFigures.classList = 'colorValor'
+      pFigures.innerText = `R$ ${listFigures[i].price}.00`
+
+      boxFigures.appendChild(imgFigures)
+      boxFigures.appendChild(tituloFigures)
+      boxFigures.appendChild(pFigures)
+      liFigures.appendChild(boxFigures)
+      secaolistFigure.appendChild(liFigures)
+   }
 
