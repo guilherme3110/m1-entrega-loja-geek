233ea2f0cce6c8696ed7c3b3b5f7987b792e10cf
guilherme3110 <61066086+guilherme3110@users.noreply.github.com>
2022-11-11 13:07:44 -0300

    correção feita

--- script.js
+++ script.js
@@ -53,55 +53,53 @@ function separaItens(listaItens){
 
 separaItens(listaItens)
 
-let secaoListPaintings = document.querySelector('.paintingsUl')
+let secaoListPaintings = document.querySelector('.frames')
   for( let i = 0 ;i < listFrames.length; i ++){
     let liFrames = document.createElement('li')
 
-    let boxItem = document.createElement('div')
+    let boxItem = document.createElement('figure')
     liFrames.appendChild(boxItem)
 
     let img = document.createElement('img')
     img.src = listFrames[i].image
 
-    let titulo = document.createElement('h3')
+    let titulo = document.createElement('p')
     titulo.classList = 'color'
     titulo.innerHTML = `<strong>${listFrames[i].name} </strong>` 
 
-    let p = document.createElement('p')
-    p.classList = 'colorValor'
-    p.innerText = `R$ ${listFrames[i].price},00`
+    let span = document.createElement('span')
+    span.classList = 'colorValor'
+    span.innerText = `R$ ${listFrames[i].price},00`
 
     boxItem.appendChild(img)
     boxItem.appendChild(titulo)
-    boxItem.appendChild(p)
-    liFrames.appendChild(boxItem)
+    boxItem.appendChild(span)
     secaoListPaintings.appendChild(liFrames)
   }
 
-  let secaolistFigure = document.querySelector('.actionFiguresUl')
+  let secaolistFigure = document.querySelector('.action')
   
    for( let i = 0; i < listFigures.length; i++){
       let liFigures = document.createElement('li')
 
-      let boxFigures = document.createElement('div')
+      let boxFigures = document.createElement('figure')
 
       liFigures.appendChild(boxFigures)
 
       let imgFigures = document.createElement('img')
       imgFigures.src = listFigures[i].image
 
-      let tituloFigures = document.createElement('h3')
+      let tituloFigures = document.createElement('p')
       tituloFigures.classList = 'color'
-      tituloFigures.innerHTML = '${listFigures[i].name}'
+      tituloFigures.innerHTML = listFigures[i].name
 
-      let pFigures = document.createElement('p')
-      pFigures.classList = 'colorValor'
-      pFigures.innerText = `R$ ${listFigures[i].price}.00`
+      let spanFigures = document.createElement('span')
+      spanFigures.classList = 'colorValor'
+      spanFigures.innerText = `R$ ${listFigures[i].price}.00`
 
       boxFigures.appendChild(imgFigures)
       boxFigures.appendChild(tituloFigures)
-      boxFigures.appendChild(pFigures)
+      boxFigures.appendChild(spanFigures)
       liFigures.appendChild(boxFigures)
       secaolistFigure.appendChild(liFigures)
    }
-
